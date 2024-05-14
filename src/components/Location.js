7fba8161074ac1b65927c56650ede13f7bae52e4
JoshmatJjen <joshmatparrot@gmail.com>
2024-05-14 15:07:05 -0400

    Push version 1

--- src/components/Location.js
+++ src/components/Location.js
@@ -2,7 +2,7 @@ import React, { useState, useContext } from 'react';
 import { AppContext } from '../context/AppContext';
 
 const Location = () => {
-    const { currency, dispatch } = useContext(AppContext);
+    const {currency, dispatch } = useContext(AppContext);
 
     const [selectedCurrency, setSelectedCurrency] = useState("Select");
 
@@ -13,17 +13,32 @@ const Location = () => {
         })
     }
 
+    const currencyLocation = (value) => {
+        switch (value) {
+            case '$' : 
+                return '$ Dollar';
+            case '£':
+                return '£ Pound';
+            case '€':
+                return '€ Euro';
+            case '₹':
+                return '₹ Ruppee';
+            default: 
+                return '£ Pound';
+        }
+    }
+
 
     return (
 
         <div class=" dropdown ">
             <a class="alert btn btn-success dropdown-toggle" style={{ background: '#84E187', borderColor: '#84E187' }} href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                Currency ({currency})
+                Currency ({currencyLocation(currency)})
             </a>
 
             <ul class="dropdown-menu" style={{ background: '#84E187' }}>
-                <li><a class="dropdown-item" href="#" onClick={() => {setSelectedCurrency("$ Dollar")}} >$ Dollar</a></li>
-                <li><a class="dropdown-item" href="#" onClick={() => {setSelectedCurrency("£ Pound")}} >£ Pound</a></li>
+                <li><a class="dropdown-item" href="#" onClick={() => setSelectedCurrency("$ Dollar")} >$ Dollar</a></li>
+                <li><a class="dropdown-item" href="#" onClick={() => setSelectedCurrency("£ Pound")} >£ Pound</a></li>
                 <li><a class="dropdown-item" href="#" onClick={() => setSelectedCurrency("€ Euro")} >€ Euro</a></li>
                 <li><a class="dropdown-item" href="#" onClick={() => setSelectedCurrency("₹ Ruppee")} >₹ Ruppee</a></li>
             </ul>
